18661133dd4194c085a497fdff0c54383078ed91
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-21 00:52:18 -0500

    CHECK HERE FOR EXAMPLE CODE

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,37 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        ZStack {
+//            //            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+//            //            RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+//            //            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+//            Button(action: {
+//                print("Edit button was tapped")
+//            }) {
+//                HStack(spacing: 10) {
+//                    Image(systemName: "pencil").renderingMode(.original)
+//                    Text("Edit")
+//                }
+//            }
+//
+//
+//
+//        }
+//    }
+//}
+
 struct ContentView: View {
+    @State private var showingAlert = false
+
     var body: some View {
-        Text("Hello, World!")
+        Button("Show Alert") {
+            self.showingAlert = true
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
